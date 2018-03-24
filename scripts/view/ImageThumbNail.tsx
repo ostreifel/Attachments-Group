b21bd5cc837fe7135e3352dff777131cae621081
Otto Streifel <ottost@microsoft.com>
2018-03-24 14:32:37 -0700

    Update thumbnail styling

--- scripts/view/ImageThumbNail.tsx
+++ scripts/view/ImageThumbNail.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { KeyCode } from "VSS/Utils/UI";
 import { IImageAttachment } from "../IImageAttachment";
 
 export interface ImageThumbNailProps {
@@ -7,6 +8,29 @@ export interface ImageThumbNailProps {
 
 export class ImageThumbNail extends React.Component<ImageThumbNailProps, {}> {
     public render() {
-        return <img className="image-thumbnail" src={this.props.image.url} />;
+        const { image } = this.props;
+        return <a
+            className="thumbnail-tile"
+            href={image.url}
+            onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                // tslint:disable-next-line:no-console
+                console.log("todo show dialog");
+            }}
+            onKeyDown={(e) => {
+                if (e.keyCode === KeyCode.ENTER) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    // tslint:disable-next-line:no-console
+                    console.log("todo show dialog");
+                }
+            }}
+        >
+            <div className="thumb-box">
+                <img className="image-thumbnail" src={image.url} />
+            </div>
+            <div className="title">{image.attributes.name}</div>
+        </a>;
     }
 }
